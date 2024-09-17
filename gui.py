fae8453c9f1fa8347ad75beb597991ab3ef14782
madesh405 <madesh6405@gmail.com>
2024-09-17 13:26:43 +0530

    made basic gui

--- gui.py
+++ gui.py
@@ -1,17 +1,16 @@
 ''' GUI for the Rock Paper Scissor game'''
 import tkinter as tk
+from random import choice
 
-
-def rock():
-    prps(0)
-def paper():
-    prps(1)
-def scissor():
-    prps(2)
+def crps():
+    weapons=["rock","paper","scissor"]
+    c=choice(weapons)
+    return c
 
 def prps(n:int) -> str:
     weapons=["rock","paper","scissor"]
-    return weapons[n]
+    c=crps()
+    print("player :",weapons[n],"\ncomputer :",c,"\n")
 
 
 
@@ -19,9 +18,9 @@ root=tk.Tk()
 root.title("Rock Paper Scissor Game")
 root.geometry('800x500')
 
-r=tk.Button(root,text="Rock",command=rock)
-p=tk.Button(root,text="Paper",command=paper)
-s=tk.Button(root,text="Scissor",command=scissor)
+r=tk.Button(root,text="Rock",command=lambda:prps(0))
+p=tk.Button(root,text="Paper",command=lambda:prps(1))
+s=tk.Button(root,text="Scissor",command=lambda:prps(2))
 r.pack()
 p.pack()
 s.pack()
